344f780c55e95446e5f25aed4846aa04f550d533
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-14 00:45:26 +1200

    commit

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home_view(*args,**kargs):
-    return HttpResponse('<h1>hello world</h1>')
+
+
+def home_view(r,*args,**kargs):
+    print(r.user ,"just came here to home" )
+   # return HttpResponse('<h1>from http response hello world</h1>')
+    return render(r, 'pages/home.html', {'title': 'home'})
+
+
+def contact_view(r,*args,**kargs):
+    return render(r, 'pages/contact.html', {'title': 'contact'})
+
+def about_view(r,*args,**kargs):
+    return render(r, 'pages/about.html', {'title': 'about'})
