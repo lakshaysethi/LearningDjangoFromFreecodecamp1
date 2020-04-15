6874cee4d57f70d57eb2c66e87ab98eb3980aeeb
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 01:22:14 +1200

    commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\KL\Desktop\LearningDjangoFromFreecodecamp1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
